2fc7e404070446602e30491c2b95b68a3b6a2fbb
Victor Macedo <victormacedo10@yahoo.com.br>
2019-03-21 15:12:24 -0300

    added gitignore

---
+++ scripts/prepareRepo.sh
@@ -0,0 +1,20 @@
+# ------------------------- BODY, FACE AND HAND MODELS -------------------------
+# Downloading body pose (COCO and MPI), face and hand models
+OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
+POSE_FOLDER="pose/"
+FACE_FOLDER="face/"
+HAND_FOLDER="hand/"
+
+# ------------------------- POSE MODELS -------------------------
+# Body (COCO)
+COCO_FOLDER=${POSE_FOLDER}"coco/"
+COCO_MODEL=${COCO_FOLDER}"pose_iter_440000.caffemodel"
+
+cd ../
+mkdir Videos
+mkdir Data
+mkdir Models
+
+#cd Models
+#wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER}
+
